d20903fc9f2cf700fded58169e9ea915e7f6a9dc
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-08-31 15:03:06 +0700

    update bom

--- src/services/engineering/bill-of-material/index.tsx
+++ src/services/engineering/bill-of-material/index.tsx
@@ -2,9 +2,10 @@ import { Axios } from '../../../configs/axios/';
 
 export const AddBillOfMaterial = (data: any) => Axios.post(process.env.BASE_URL+'/bom', data);
 export const GetBillOfMaterial = (page: number, perpage: number) => Axios.get(process.env.BASE_URL+`/bom?page=${page}&limit=${perpage}`);
+export const EditBillOfMaterial = (data: any) => Axios.put(process.env.BASE_URL+`/bom`, data);
+export const DeleteDetailBillOfMaterial = (id: string) => Axios.delete(process.env.BASE_URL+`/bomDetail/${id}`);
 // export const GetSchedulleDrawing = () => Axios.get(process.env.BASE_URL+`/tmsdrawing`);
 // export const DeleteSummary = (id: string) => Axios.delete(process.env.BASE_URL+`/summary/${id}`);
-// export const EditSummary = (data: any, id: string) => Axios.put(process.env.BASE_URL+`/summary/${id}`, data);
 // export const EditSummaryDetail = (data: any) => Axios.put(process.env.BASE_URL+`/summaryDetail`, data);
 
 // export const GetSummaryDrawing = () => Axios.get(process.env.BASE_URL+`/SummaryBom`);
