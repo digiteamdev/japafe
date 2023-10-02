0ba719b4f45a2800262d9c3e57850fe7435b7265
Tedi Suryadi <tedi.suryadi77@gmail.com>
2023-10-02 14:29:53 +0700

    approval sr

---
+++ src/services/purchasing-logistic/approval-sr/index.tsx
@@ -0,0 +1,8 @@
+import { Axios } from '../../../configs/axios/';
+
+// export const AddActivity = (data: any) => Axios.post(process.env.BASE_URL+'/masterAktivitas', data);
+export const GetApprovalSr = (page: number, perpage: number) => Axios.get(process.env.BASE_URL+`/SRapprove?page=${page}&limit=${perpage}`);
+export const GetSrValid = () => Axios.get(process.env.BASE_URL+`/SRapprove`);
+export const ApprovalSr = (data: any) => Axios.put(process.env.BASE_URL+`/SRapprove`, data);
+// export const DeleteActivity = (id: string) => Axios.delete(process.env.BASE_URL+`/masterAktivitas/${id}`);
+export const SearchApprovalSr = (page: number, perpage: number, search: string) => Axios.get(process.env.BASE_URL+`/SRapprove?page=${page}&limit=${perpage}&search=${search}`);
