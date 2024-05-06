1349bc3888d4e5e8e440719312a4d66ec5dd93e6
Tedi Suryadi <tedi.suryadi77@gmail.com>
2024-05-06 10:57:18 +0700

    pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
+const nextConfig = withPWA({
+  reactStrictMode: false,
+	env: {
     BASE_URL: process.env.base_url
   },
   images: {
     domains: ['res.cloudinary.com'],
-  }
-}
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
